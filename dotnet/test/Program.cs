b90b0100dc2e9d788c8bba78509ef501e0f78aa1
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-04-19 10:59:00 +0900

    dotnet experiment

---
+++ dotnet/test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Dll
+{
+    [DllImport("libc.so.6")]
+    public static extern int getpid();
+}
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pid is " + Dll.getpid());
+        }
+    }
+}
