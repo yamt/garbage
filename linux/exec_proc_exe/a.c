a70b7e1c5b82492654336cbad6b0c7bdf2ac20d1
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-05-11 17:15:21 +0900

    try readlink as well

--- linux/exec_proc_exe/a.c
+++ linux/exec_proc_exe/a.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,17 +7,27 @@
 
 extern char **environ;
 
+const char *path = "/proc/self/exe";
+
 int
 main(int argc, char *argv[])
 {
+    char buf[PATH_MAX];
     int ret;
+
+    ret = readlink(path, buf, sizeof(buf));
+    if (ret == -1) {
+        printf("readlink failed with %d, errno=%d\n", ret, errno);
+    } else {
+        printf("link contents: %.*s\n", ret, buf);
+    }
 
     printf("argv[0] = %s\n", argv[0]);
     if (!strcmp(argv[0], "hoge")) {
         exit(0);
     }
     argv[0] = "hoge";
-    ret = execve("/proc/self/exe", argv, environ);
+    ret = execve(path, argv, environ);
     printf("execve returned with %d, errno=%d\n", ret, errno);
     exit(1);
 }
