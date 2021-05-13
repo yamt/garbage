40a8e4c3749551c9bcbb3bf37a6ec79b586b4327
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-05-13 09:40:05 +0900

    exec_proc_exe: examine a few more things

--- linux/exec_proc_exe/a.c
+++ linux/exec_proc_exe/a.c
@@ -7,7 +7,7 @@
 
 extern char **environ;
 
-const char *path = "/proc/self/exe";
+const char *proc_self_exe = "/proc/self/exe";
 
 int
 main(int argc, char *argv[])
@@ -15,19 +15,35 @@ main(int argc, char *argv[])
     char buf[PATH_MAX];
     int ret;
 
-    ret = readlink(path, buf, sizeof(buf));
+    printf("argv[0] = %s\n", argv[0]);
+
+    ret = readlink(proc_self_exe, buf, sizeof(buf));
     if (ret == -1) {
-        printf("readlink failed with %d, errno=%d\n", ret, errno);
+        printf("readlink %s failed with %d, errno=%d\n",
+           proc_self_exe, ret, errno);
     } else {
-        printf("link contents: %.*s\n", ret, buf);
+        printf("readlink %s contents: %.*s\n", proc_self_exe, ret, buf);
     }
 
-    printf("argv[0] = %s\n", argv[0]);
-    if (!strcmp(argv[0], "hoge")) {
+    if (!strcmp(argv[0], "step3")) {
         exit(0);
     }
-    argv[0] = "hoge";
+
+    const char *path;
+
+    if (strcmp(argv[0], "step2")) {
+        path = argv[0];
+        argv[0] = "step2";
+    } else {
+	const char *tmp = "/tmp";
+	ret = chdir(tmp); /* try to confuse qemu */
+	if (ret == -1) {
+	    printf("chdir %s failed with %d, errno=%d\n", tmp, ret, errno);
+	}
+        path = proc_self_exe;
+        argv[0] = "step3";
+    }
     ret = execve(path, argv, environ);
-    printf("execve returned with %d, errno=%d\n", ret, errno);
+    printf("execve %s returned with %d, errno=%d\n", path, ret, errno);
     exit(1);
 }
