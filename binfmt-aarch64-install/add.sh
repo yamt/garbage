c8bbb68b1ef54f1447aa1f09a08262d83b87704b
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-05-13 10:29:14 +0900

    rename our entry to be unique
    also, remove possibly conflicting entries

--- binfmt-aarch64-install/add.sh
+++ binfmt-aarch64-install/add.sh
@@ -2,15 +2,34 @@
 
 set -e
 
+OUR_ENTRY=qemu-aarch64-yamt
+
 BINFMT_MISC_MOUNT=/proc/sys/fs/binfmt_misc
 REGISTER=${BINFMT_MISC_MOUNT}/register
 if [ ! -f ${REGISTER} ]; then
     mount binfmt_misc -t binfmt_misc ${BINFMT_MISC_MOUNT}
 fi
 
-DATA=':qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/yamt/qemu-aarch64:FPO'
+echo "Listing existing entries..."
+ls ${BINFMT_MISC_MOUNT}
 
-echo -1 > ${BINFMT_MISC_MOUNT}/qemu-aarch64
+echo "Removing possibly conflicting entries..."
+# ours
+ENTRIES=$OUR_ENTRY
+# docker desktop
+ENTRIES="$ENTRIES qemu-aarch64"
+# multiarch/qemu-user-static
+ENTRIES="$ENTRIES qemu-aarch64-static"
+for e in $ENTRIES; do
+    ENTRY=${BINFMT_MISC_MOUNT}/$e
+    if [ -f ${ENTRY} ]; then
+        echo "Removing $e..."
+        echo -1 > ${ENTRY}
+    fi
+done
+
+DATA=":${OUR_ENTRY}:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/yamt/qemu-aarch64:FPO"
 echo ${DATA} > ${REGISTER}
 
-cat ${BINFMT_MISC_MOUNT}/qemu-aarch64
+echo "Registered entry:"
+cat ${BINFMT_MISC_MOUNT}/${OUR_ENTRY}
