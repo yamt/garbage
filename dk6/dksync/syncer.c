0c0891e2408e6404f7f44425db719d188daa23ee
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-08-08 16:41:15 +0900

    dksync: try to decode a few commands

--- dk6/dksync/syncer.c
+++ dk6/dksync/syncer.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <inttypes.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -14,6 +15,8 @@
 #define LAYER_PRESS_CMD 0x3e
 #define LAYER_DEPRESS_CMD 0x3c
 #define SYNC_CMD 0x46
+#define KEY_ADDED_CMD 0x18
+#define KEY_REMOVED_CMD 0x1a
 
 #define MAX_DEVICES 2
 #define MAX_PACKET_SIZE 64
@@ -87,12 +90,33 @@ syncer_thread(void *vp)
                 }
                 assert(ret == sizeof(buf));
                 dump_packet(buf, ret, k->name, "read");
+
+                uint8_t cmd = buf[0];
+                uint32_t keyid;
+                const char *cmd_str;
                 unsigned char press;
-                if (buf[0] == LAYER_PRESS_CMD) {
+
+                switch (cmd) {
+                case KEY_ADDED_CMD:
+                case KEY_REMOVED_CMD:
+                        keyid = (buf[2] << 24) + (buf[3] << 16) +
+                                (buf[4] << 8) + (buf[5] << 0);
+                        cmd_str = cmd == KEY_ADDED_CMD ? "KEY ADDED"
+                                                       : "KEY REMOVED";
+                        fprintf(stderr,
+                                "%s: %s: "
+                                "ID=%08" PRIx32 " "
+                                "layers=%02x/%02x/%02x/%02x\n",
+                                k->name, cmd_str, keyid, buf[7], buf[8],
+                                buf[9], buf[10]);
+                        continue;
+                case LAYER_PRESS_CMD:
                         press = 0x03;
-                } else if (buf[0] == LAYER_DEPRESS_CMD) {
+                        break;
+                case LAYER_DEPRESS_CMD:
                         press = 0x02;
-                } else {
+                        break;
+                default:
                         continue;
                 }
                 unsigned char id = buf[1];
