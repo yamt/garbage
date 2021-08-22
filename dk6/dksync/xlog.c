527725216b5488ae95ece6bde958d6579d864ad7
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-08-22 17:38:27 +0900

    add timestamp

--- dk6/dksync/xlog.c
+++ dk6/dksync/xlog.c
@@ -1,14 +1,19 @@
+#include <inttypes.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "xlog.h"
 
 void
 xlog_printf(const char *fmt, ...)
 {
+        struct timespec ts;
         va_list ap;
 
+        clock_gettime(CLOCK_REALTIME, &ts);
         va_start(ap, fmt);
+        fprintf(stderr, "%ju.%09ld: ", (uintmax_t)ts.tv_sec, ts.tv_nsec);
         vfprintf(stderr, fmt, ap);
         va_end(ap);
 }
