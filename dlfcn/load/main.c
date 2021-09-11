78fb5885e7292f4d4b48f3ccc3484ae5f9258c6b
YAMAMOTO Takashi <imuwoto@gmail.com>
2021-09-11 10:09:36 +0900

    nicer errors

--- dlfcn/load/main.c
+++ dlfcn/load/main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <dlfcn.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void
 exported_from_main(const char *msg)
@@ -13,8 +14,14 @@ main(int argc, char *argv[])
 {
         const char *name = argv[1];
         void *h = dlopen(name, RTLD_NOW);
-        assert(h != NULL);
+        if (h == NULL) {
+                fprintf(stderr, "dlopen: %s\n", dlerror());
+                exit(EXIT_FAILURE);
+        }
         void (*module_func)(const char *) = dlsym(h, "module_func");
-        assert(module_func != NULL);
+        if (module_func == NULL) {
+                fprintf(stderr, "dlsym: %s\n", dlerror());
+                exit(EXIT_FAILURE);
+        }
         module_func("hey");
 }
