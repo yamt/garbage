577788ffdee9b1e1fe065f00fb363462f9bedd16
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-01-12 22:32:49 +0900

    wip

--- objc/2/app_delegate.m
+++ objc/2/app_delegate.m
@@ -5,6 +5,13 @@
 - (void)applicationDidFinishLaunching: (NSNotification *)aNotification
 {
 	NSLog(@"applicationDidFinishLaunching");
+
+	NSRect rect = NSMakeRect(0, 0, 500, 500);
+    NSUInteger style = NSTitledWindowMask | NSClosableWindowMask;
+    NSWindow *window = [NSWindow alloc];
+    [window initWithContentRect: rect styleMask: style backing: NSBackingStoreBuffered defer: false];
+	[window setTitle: @"Hello"];
+    [window makeKeyAndOrderFront: self];
 }
 
 @end
