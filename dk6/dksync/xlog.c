3d23773c0d9e8ad8b3dd48b506d14fdbf4fdbc2c
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-01-21 18:35:32 +0900

    xlog: print localtime

--- dk6/dksync/xlog.c
+++ dk6/dksync/xlog.c
@@ -12,8 +12,15 @@ xlog_printf(const char *fmt, ...)
         va_list ap;
 
         clock_gettime(CLOCK_REALTIME, &ts);
+
+        time_t clock = ts.tv_sec;
+        struct tm tm;
+        char buf[sizeof("0000-00-00 00-00-00")];
+        strftime(buf, sizeof(buf), "%F %T", localtime_r(&clock, &tm));
+
         va_start(ap, fmt);
-        fprintf(stderr, "%ju.%09ld: ", (uintmax_t)ts.tv_sec, ts.tv_nsec);
+        fprintf(stderr, "%s (%ju.%09ld): ", buf, (uintmax_t)ts.tv_sec,
+                ts.tv_nsec);
         vfprintf(stderr, fmt, ap);
         va_end(ap);
 }
