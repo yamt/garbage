d5c1b6257b1d6a534d20bcd126191e9c88227a3f
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-05-18 20:53:00 +0900

    CP210x

--- ioreg/find_serial.py
+++ ioreg/find_serial.py
@@ -6,14 +6,22 @@ import plistlib
 device_io_object_class = sys.argv[1]
 device_b_interface_number = int(sys.argv[2])
 
+
 def visit(o):
-    for c in o.get('IORegistryEntryChildren', []):
-        if o.get('IOObjectClass') == device_io_object_class and \
-           o['bInterfaceNumber'] == device_b_interface_number and \
-           c.get('IOObjectClass') == 'IOSerialBSDClient':
-            print(c['IODialinDevice'])
+    for c in o.get("IORegistryEntryChildren", []):
+        if (
+            o.get("IOObjectClass") == device_io_object_class
+            and o.get("bInterfaceNumber") == device_b_interface_number
+        ):
+            if c.get("IOObjectClass") == "IOSerialBSDClient":
+                print(c["IODialinDevice"])
+            elif c.get("IOObjectClass") == "IORS232SerialStreamSync":
+                print(c["IORegistryEntryChildren"][0]["IODialinDevice"])
+            else:
+                visit(c)
         else:
             visit(c)
+
 
 list = plistlib.load(sys.stdin.buffer, fmt=plistlib.FMT_XML)
 for c in list:
