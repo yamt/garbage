0136c633ae264cc89eb0fb5da28902b676ee5e99
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-06-01 22:30:25 +0900

    specify sdk explicitly

--- macos/gai_leak/a.sh
+++ macos/gai_leak/a.sh
@@ -1,12 +1,19 @@
 
+set -e
+
+SDK=$(xcrun --sdk macosx --show-sdk-path)
+
 sw_vers
 uname -a
 
 # homebrew-installed llvm
-CC=/usr/local/Cellar/llvm@12/12.0.1_1/bin/clang
+#CC=/usr/local/Cellar/llvm@12/12.0.1_1/bin/clang
 #CC=/usr/local/Cellar/llvm/13.0.1/bin/clang
+CC=/usr/local/opt/llvm@13/bin/clang
+#CC=${HOME}/llvm/bin/clang
+#CC=${HOME}/git/llvm-project/b/bin/clang
 ${CC} --version
-${CC} -fsanitize=address a.c
+${CC} -isysroot ${SDK} -fsanitize=address a.c
 export ASAN_OPTIONS=detect_leaks=1
 
 ./a.out ipv4.google.com https
