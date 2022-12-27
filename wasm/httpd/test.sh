02d72a4e87d67e9901d4ad171fccb047bcb01de6
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-12-27 23:43:16 +0900

    wasm on wasm example

--- wasm/httpd/test.sh
+++ wasm/httpd/test.sh
@@ -2,5 +2,7 @@
 
 ../../listenexec/listenexec -p 9999 ~/git/toywasm/b/toywasm --wasi --wasi-dir=. httpd.wasm
 
+# ../../listenexec/listenexec -p 9999 ~/git/toywasm/b/toywasm --wasi --wasi-dir=. --wasi-dir=${HOME}/git/toywasm/build.wasm -- ${HOME}/git/toywasm/build.wasm/toywasm --wasi -- httpd.wasm
+
 # eg.
 # ab -c5 -n10000 "http://[::1]:9999/Makefile"
