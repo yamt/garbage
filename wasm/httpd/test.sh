3618fbf5f5c9a6bdd28e950f4d240eadd52ef7c9
YAMAMOTO Takashi <imuwoto@gmail.com>
2022-12-28 20:02:41 +0900

    pass through arguments

--- wasm/httpd/test.sh
+++ wasm/httpd/test.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-../../listenexec/listenexec -p 9999 ~/git/toywasm/b/toywasm --wasi --wasi-dir=. httpd.wasm
+../../listenexec/listenexec -p 9999 ~/git/toywasm/b/toywasm --wasi --wasi-dir=. -- httpd.wasm "$@"
 
 # ../../listenexec/listenexec -p 9999 ~/git/toywasm/b/toywasm --wasi --wasi-dir=. --wasi-dir=${HOME}/git/toywasm/build.wasm -- ${HOME}/git/toywasm/build.wasm/toywasm --wasi -- httpd.wasm
 
