de35c32755648c24a587f9bb4d92e4a3e638b8cc
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-04-18 22:54:48 +0900

    style

--- wasm/stat/a.c
+++ wasm/stat/a.c
@@ -3,9 +3,9 @@
 int
 main()
 {
-	__wasi_fdstat_t buf;
-    int r = __wasi_fd_fdstat_get(0, &buf);
-    printf("__wasi_fd_fdstat_get returned %d\n", r);
-    printf("fs_filetype %jx\n", (uintmax_t)buf.fs_filetype);
-    printf("fs_rights_base %jx\n", (uintmax_t)buf.fs_rights_base);
+        __wasi_fdstat_t buf;
+        int r = __wasi_fd_fdstat_get(0, &buf);
+        printf("__wasi_fd_fdstat_get returned %d\n", r);
+        printf("fs_filetype %jx\n", (uintmax_t)buf.fs_filetype);
+        printf("fs_rights_base %jx\n", (uintmax_t)buf.fs_rights_base);
 }
