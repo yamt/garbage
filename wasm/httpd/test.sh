3ff00158f7940b2f35de7f5f616b881adba98657
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-06-20 21:48:21 +0900

    wasmtime example

--- wasm/httpd/test.sh
+++ wasm/httpd/test.sh
@@ -9,5 +9,10 @@
 # iwasm
 # ../../listenexec/listenexec -p 9999 ~/git/wasm-micro-runtime/product-mini/platforms/darwin/b.thread/iwasm --max-threads=16 --dir=. httpd.wasm
 
+# wasmtime
+# ../../listenexec/listenexec -s -p 9999 wasmtime --listenfd --dir=. httpd.wasm
+# ../../listenexec/listenexec -s -p 9999 wasmtime --wasm-features=threads --wasi-modules=experimental-wasi-threads --listenfd --dir=. httpd.wasm
+# note: wasmtime bails out if we give it a socket as stdin
+
 # eg.
 # ab -c5 -n10000 "http://[::1]:9999/Makefile"
