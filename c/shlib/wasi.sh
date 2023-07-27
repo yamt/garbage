ccba47a4002c08e2c319b8d4457f074362ce1874
YAMAMOTO Takashi <imuwoto@gmail.com>
2023-07-27 22:18:03 +0900

    a few more experimantal things

--- c/shlib/wasi.sh
+++ c/shlib/wasi.sh
@@ -7,25 +7,35 @@ set -x
 WASI_SDK=${WASI_SDK:-/Volumes/PortableSSD/git/component-linking-demo/wasi-sdk/build/install/opt/wasi-sdk}
 CC=${WASI_SDK}/bin/clang
 
-WASI_SYSROOT=${WASI_SDK}/share/wasi-sysroot
+#WASI_SYSROOT=${WASI_SDK}/share/wasi-sysroot
+WASI_SYSROOT=/Users/yamamoto/git/wasi-libc/sysroot
+
 #LLVM_HOME=/Volumes/PortableSSD/llvm/llvm
-#CC=${LLVM_HOME}/bin/clang
-#CFLAGS="${CFLAGS} --sysroot ${WASI_SYSROOT}"
+LLVM_HOME=/Volumes/PortableSSD/llvm/build
+#RESOURCE_DIR=/Volumes/PortableSSD/llvm/llvm/lib/clang/17
+RESOURCE_DIR=${WASI_SDK}/lib/clang/17
+CC=${LLVM_HOME}/bin/clang
+CFLAGS="${CFLAGS} --sysroot ${WASI_SYSROOT}"
+CFLAGS="${CFLAGS} -resource-dir ${RESOURCE_DIR}"
 
 # built with TOYWASM_ENABLE_DYLD=ON
-TOYWASM=${TOYWASM:-/Users/yamamoto/git/toywasm/b/toywasm}
+TOYWASM=${TOYWASM:-toywasm}
 
 CFLAGS="${CFLAGS} -O3 -fPIC"
 
 # https://reviews.llvm.org/D155542
-# CFLAGS="${CFLAGS} -mextended-const"
+#CFLAGS="${CFLAGS} -mextended-const"
 
 # CFLAGS="${CFLAGS} -mtail-call"
 
 CLINKFLAGS="-Xlinker --experimental-pic"
+#CLIBLINKFLAGS="-shared -fvisibility=default -mexec-model=reactor"
+CLIBLINKFLAGS="-shared -fvisibility=default"
 
-${CC} ${CFLAGS} ${CLINKFLAGS} -shared -fvisibility=default -o libbar.so bar.c
-${CC} ${CFLAGS} ${CLINKFLAGS} -shared -fvisibility=default -o libfoo.so foo.c
+#CRT1=$(${CC} --print-file-name crt1-reactor.o)
+
+${CC} ${CFLAGS} ${CLINKFLAGS} ${CLIBLINKFLAGS} -o libbar.so bar.c
+${CC} ${CFLAGS} ${CLINKFLAGS} ${CLIBLINKFLAGS} -o libfoo.so foo.c
 ${CC} ${CFLAGS} ${CLINKFLAGS} \
 -Xlinker -pie \
 -Xlinker --export-if-defined=__main_argc_argv \
