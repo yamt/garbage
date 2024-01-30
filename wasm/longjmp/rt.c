259b11d704b767b73428ccd7eaca3e7ce066cda0
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-01-30 23:08:19 +0900

    a bit more complete implementation

--- wasm/longjmp/rt.c
+++ wasm/longjmp/rt.c
@@ -2,36 +2,69 @@
  * https://github.com/llvm/llvm-project/blob/main/llvm/lib/Target/WebAssembly/WebAssemblyLowerEmscriptenEHSjLj.cpp
  */
 
-struct {
-        void *env;
-        int val;
-} arg; /* XXX should use a thread-local? */
+#include <stdint.h>
 
-int g_label; /* XXX single setjmp call-site is assumed for now */
+struct entry {
+        uint32_t id;
+        uint32_t label;
+};
+
+struct state {
+        uint32_t id;
+        uint32_t size;
+        struct arg {
+                void *env;
+                int val;
+        } arg;
+} g_state; /* XXX should be thread-local */
 
 void *
-saveSetjmp(void *env, int label, void *table, int size)
+saveSetjmp(void *env, uint32_t label, void *table, uint32_t size)
 {
-        g_label = label;
+        struct state *state = &g_state;
+        struct entry *e = table;
+        uint32_t i;
+        for (i = 0; i < size; i++) {
+                if (e[i].id == 0) {
+                        uint32_t id = ++state->id;
+                        *(uint32_t *)env = id;
+                        e[i].id = id;
+                        e[i].label = label;
+                        goto done;
+                }
+        }
+        /* TODO grow the table */
+done:
+        state->size = size;
         return table;
 }
 
 int
-testSetjmp(int id, void *table, int size)
+testSetjmp(unsigned int id, void *table, uint32_t size)
 {
-        return g_label;
+        struct entry *e = table;
+        uint32_t i;
+        for (i = 0; i < size; i++) {
+                if (e[i].id == id) {
+                        return e[i].label;
+                }
+        }
+        return 0;
 }
 
 int
 getTempRet0()
 {
-        return 0; /* XXX should use a thread-local? */
+        struct state *state = &g_state;
+        return state->size;
 }
 
 void
 __wasm_longjmp(void *env, int val)
 {
-        arg.env = env;
-        arg.val = val;
-        __builtin_wasm_throw(1, &arg);
+        struct state *state = &g_state;
+        struct arg *arg = &state->arg;
+        arg->env = env;
+        arg->val = val;
+        __builtin_wasm_throw(1, arg);
 }
