79c382152a0592469b1a749699e4290515ba60f4
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-02-09 15:47:22 +0900

    poc of alternative wasm sjlj abi

--- wasm/longjmp/rt.c
+++ wasm/longjmp/rt.c
@@ -5,69 +5,39 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-struct entry {
-        uint32_t id;
+struct jmp_buf_impl {
+        void *func_invocation_id;
         uint32_t label;
 };
 
 static _Thread_local struct state {
-        uint32_t id;
-        uint32_t size;
         struct arg {
                 void *env;
                 int val;
         } arg;
 } g_state;
 
-/*
- * table is allocated at the entry of functions which call setjmp.
- *
- *   table = malloc(40);
- *   size = 4;
- *   *(int *)table = 0;
- */
-_Static_assert(sizeof(struct entry) * (4 + 1) <= 40, "entry size");
-
 void *
 saveSetjmp(void *env, uint32_t label, void *table, uint32_t size)
 {
-        struct state *state = &g_state;
-        struct entry *e = table;
-        uint32_t i;
-        for (i = 0; i < size; i++) {
-                if (e[i].id == 0) {
-                        uint32_t id = ++state->id;
-                        *(uint32_t *)env = id;
-                        e[i].id = id;
-                        e[i].label = label;
-                        /*
-                         * note: only the first word is zero-initialized
-                         * by the caller.
-                         */
-                        e[i + 1].id = 0;
-                        goto done;
-                }
-        }
-        size *= 2;
-        void *p = realloc(table, sizeof(*e) * (size + 1));
-        if (p == NULL) {
+        struct jmp_buf_impl *jb = env;
+        if (label == 0) { /* ABI contract */
                 __builtin_trap();
         }
-        table = p;
-done:
-        state->size = size;
+        jb->func_invocation_id = table;
+        jb->label = label;
         return table;
 }
 
 uint32_t
-testSetjmp(unsigned int id, void *table, uint32_t size)
+testSetjmp(void *env, void *table, uint32_t size)
 {
-        struct entry *e = table;
-        uint32_t i;
-        for (i = 0; i < size; i++) {
-                if (e[i].id == id) {
-                        return e[i].label;
-                }
+        struct jmp_buf_impl *jb = env;
+        if (jb->label == 0) { /* ABI contract */
+                __builtin_trap();
+        }
+        if (jb->func_invocation_id == table) {
+                return jb->label;
         }
         return 0;
 }
@@ -75,8 +45,7 @@ testSetjmp(unsigned int id, void *table, uint32_t size)
 uint32_t
 getTempRet0()
 {
-        struct state *state = &g_state;
-        return state->size;
+        return 0;
 }
 
 void
