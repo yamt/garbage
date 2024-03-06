b485fddf7a2f6b5c6b94d8d3b28a18649dedc433
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-03-06 13:25:55 +0900

    separate setjmp.h

--- wasm/longjmp/a.c
+++ wasm/longjmp/a.c
@@ -3,25 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#if defined(__wasm__)
-/* for some reasons, __builtin_setjmp/__builtin_longjmp is not used */
-typedef void *jmp_buf[4];
-int setjmp(jmp_buf env) __attribute__((__returns_twice__));
-void longjmp(jmp_buf env, int val);
-#else
-#if 1
-#include <setjmp.h>
-#else
-/*
- * note: a jmp_buf for __builtin_setjmp is of 5 words
- * https://gcc.gnu.org/onlinedocs/gcc/Nonlocal-Gotos.html
- * https://llvm.org/docs/ExceptionHandling.html#llvm-eh-sjlj-setjmp
- */
-typedef void *jmp_buf[5];
-#define setjmp __builtin_setjmp
-#define longjmp __builtin_longjmp
-#endif
-#endif
+#include "setjmp.h"
 
 jmp_buf buf1;
 jmp_buf buf2;
---
+++ wasm/longjmp/setjmp.h
@@ -0,0 +1,19 @@
+#if defined(__wasm__)
+/* for some reasons, __builtin_setjmp/__builtin_longjmp is not used */
+typedef void *jmp_buf[4];
+int setjmp(jmp_buf env) __attribute__((__returns_twice__));
+void longjmp(jmp_buf env, int val);
+#else
+#if 1
+#include <setjmp.h>
+#else
+/*
+ * note: a jmp_buf for __builtin_setjmp is of 5 words
+ * https://gcc.gnu.org/onlinedocs/gcc/Nonlocal-Gotos.html
+ * https://llvm.org/docs/ExceptionHandling.html#llvm-eh-sjlj-setjmp
+ */
+typedef void *jmp_buf[5];
+#define setjmp __builtin_setjmp
+#define longjmp __builtin_longjmp
+#endif
+#endif
