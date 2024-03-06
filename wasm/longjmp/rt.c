6d81bcde078f81298f4e562a783434591d7c4e72
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-03-06 14:31:31 +0900

    rename a few variables

--- wasm/longjmp/rt.c
+++ wasm/longjmp/rt.c
@@ -27,30 +27,30 @@ struct jmp_buf_impl {
 };
 
 void
-__wasm_sjlj_setjmp(void *env, uint32_t label, void *table)
+__wasm_sjlj_setjmp(void *env, uint32_t label, void *func_invocation_id)
 {
         struct jmp_buf_impl *jb = env;
         if (label == 0) { /* ABI contract */
                 __builtin_trap();
         }
-        if (table == NULL) { /* sanity check */
+        if (func_invocation_id == NULL) { /* sanity check */
                 __builtin_trap();
         }
-        jb->func_invocation_id = table;
+        jb->func_invocation_id = func_invocation_id;
         jb->label = label;
 }
 
 uint32_t
-__wasm_sjlj_test(void *env, void *table)
+__wasm_sjlj_test(void *env, void *func_invocation_id)
 {
         struct jmp_buf_impl *jb = env;
         if (jb->label == 0) { /* ABI contract */
                 __builtin_trap();
         }
-        if (table == NULL) { /* sanity check */
+        if (func_invocation_id == NULL) { /* sanity check */
                 __builtin_trap();
         }
-        if (jb->func_invocation_id == table) {
+        if (jb->func_invocation_id == func_invocation_id) {
                 return jb->label;
         }
         return 0;
