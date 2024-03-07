54494982d977c1004d09a48e40ddd8d6a74b69a1
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-03-07 13:05:34 +0900

    test_emcc.sh: remove rt.c bits

--- wasm/longjmp/test_emcc.sh
+++ wasm/longjmp/test_emcc.sh
@@ -10,12 +10,9 @@ a.c lib.c
 node a.out.js
 
 emcc -sSIDE_MODULE -sSUPPORT_LONGJMP=wasm -Os \
--o rt.so rt.c
-emcc -sSIDE_MODULE -sSUPPORT_LONGJMP=wasm -Os \
 -o lib.so lib.c
-
 emcc -sMAIN_MODULE=1 -sSUPPORT_LONGJMP=wasm -Os \
 -sERROR_ON_UNDEFINED_SYMBOLS=0 \
-a.c -L. lib.so rt.so
+a.c -L. lib.so
 
 node a.out.js
