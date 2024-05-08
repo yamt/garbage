1d4ca29a31a711185eefd6291315cf5ed9afbcf9
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-05-09 01:55:30 +0900

    stackfill
    * try to make a bit more stable estimate
    * nuttx

--- c/stackfill/main.c
+++ c/stackfill/main.c
@@ -12,21 +12,28 @@ const size_t margin = 32; /* for the frame of memset */
 __attribute__((noinline)) void
 fill(unsigned char *p)
 {
-        int var;
-        assert(p < (unsigned char *)&var);
-        unsigned char *sp = (unsigned char *)&var - margin;
+        unsigned char *fp = __builtin_frame_address(0);
+        assert(p < fp);
+        unsigned char *sp = fp - margin;
         assert(p < sp);
         size_t sz = sp - p;
 
         memset(p, magic, sz);
 }
 
+/*
+ * count the number of bytes left unmodified since fill().
+ * the intention here is to count the unused stack bytes.
+ *
+ * XXX imprecise for windowed abis like xtensa because we have no controls
+ * on register spills.
+ */
 __attribute__((noinline)) size_t
 count(const unsigned char *p)
 {
-        int var;
-        assert(p < (unsigned char *)&var);
-        unsigned char *sp = (unsigned char *)&var - margin;
+        unsigned char *fp = __builtin_frame_address(0);
+        assert(p < fp);
+        unsigned char *sp = fp - margin;
         assert(p < sp);
         size_t sz = sp - p;
 
@@ -39,17 +46,35 @@ count(const unsigned char *p)
         return i;
 }
 
+__attribute__((noinline)) unsigned char *
+return_fp(void)
+{
+        return __builtin_frame_address(0);
+}
+
+__attribute__((noinline)) size_t
+min_frame_size(void)
+{
+        const unsigned char *fp1 = return_fp();
+        const unsigned char *fp = __builtin_frame_address(0);
+        return fp - fp1;
+}
+
 char dummy[1000];
 
 int
-main()
+main(void)
 {
-#if defined(__APPLE__)
+#if defined(__APPLE__) || defined(__NuttX__)
         pthread_t self = pthread_self();
         unsigned char *addr = pthread_get_stackaddr_np(self);
         size_t sz = pthread_get_stacksize_np(self);
-        assert((unsigned char *)&addr < addr);
-        unsigned char *base = addr - sz;
+        unsigned char *base;
+        if ((unsigned char *)&addr < (unsigned char *)addr) {
+                base = addr - sz;
+        } else {
+                base = addr;
+        }
         assert(base < (unsigned char *)&addr);
 #endif
 #if defined(__linux__)
@@ -71,10 +96,19 @@ main()
         sz -= gsz;
 #endif
 #endif
+        const unsigned char *fp = __builtin_frame_address(0);
         /* ensure to bind memset */
         memset(&dummy, 1, 1000);
         printf("dummy %u\n", dummy[0]);
-        printf("base %p addr %p sp %p sz %zu\n", base, addr, &addr, sz);
+        printf("fp %p base %p addr %p sp %p sz %zu\n", fp, base, addr, &addr,
+               sz);
+        printf("&addr - base = %zu\n",
+               (unsigned char *)&addr - (unsigned char *)base);
+        printf("margin %zu\n", margin);
+        printf("min call frame size %zu\n", min_frame_size());
+        printf("base sp %p\n", return_fp() - min_frame_size());
+        size_t remain_base = return_fp() - min_frame_size() - base;
+        printf("remaining stack before calls %zu\n", remain_base);
 
         /*
          * run it three times to see the first call overhead.
@@ -90,10 +124,12 @@ main()
                  * sleep was chosen here because it's unlikely used
                  * earlier in this program. (eg. in crt)
                  */
-                sleep(0);
+                sleep(1);
 
                 size_t c1 = count(base);
-                printf("c0 %zu (%zu) c1 %zu (%zu) diff %zd\n", c0, sz - c0, c1,
+                printf("c0 %zu (%zu %zu) c1 %zu (%zu %zu) diff %zd\n", c0,
+                       remain_base - c0, sz - c0, c1, remain_base - c1,
                        sz - c1, c0 - c1);
         }
+        return 0;
 }
