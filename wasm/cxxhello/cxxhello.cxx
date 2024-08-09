56846b0d58753fd7519c1d15cdf3af55eb76206f
YAMAMOTO Takashi <imuwoto@gmail.com>
2024-08-09 23:43:11 +0900

    use lto and mergefunc

--- wasm/cxxhello/cxxhello.cxx
+++ wasm/cxxhello/cxxhello.cxx
@@ -3,5 +3,17 @@
 int
 main()
 {
+#if 1
         std::cout << "hello C++" << std::endl;
+#endif
+#if 0
+        new char[1024*1024*16];
+#endif
 }
+
+#if 0
+extern "C" void abort_message(const char *fmt, ...)
+{
+	//fprintf(stderr, "my abort message\n");
+}
+#endif
