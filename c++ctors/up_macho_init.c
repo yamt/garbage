0cdd28634063acb28c0cb5f9bd4449e844d5a906
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-02-10 22:54:08 +0900

    sync with the latest nuttx version

--- c++ctors/up_macho_init.c
+++ c++ctors/up_macho_init.c
@@ -1,5 +1,7 @@
 /****************************************************************************
- * arch/sim/src/sim/up_macho_init.c
+ * arch/sim/src/sim/posix/sim_macho_init.c
+ *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -22,8 +24,11 @@
  * Included Files
  ****************************************************************************/
 
+#include <sys/mman.h>
+
 #include <assert.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /****************************************************************************
  * Private Data
@@ -49,21 +54,51 @@ static const char **g_saved_argv;
 static const char **g_saved_envp;
 static const char **g_saved_apple;
 
+static void
+allow_write(const void *start, const void *end)
+{
+  const size_t page_size = sysconf(_SC_PAGE_SIZE);
+  const size_t page_mask = ~(page_size - 1);
+  void *p = (void *)((uintptr_t)start & page_mask);
+  size_t sz = ((uintptr_t)end - (uintptr_t)p + page_size - 1) & ~page_mask;
+
+  /* It seems that Monterey (12.1) maps the section read-only.
+   * Make it writable as we want to patch it.
+   * This was not necessary for Mojave.
+   * Ignore failures as this might not be critical, depending on
+   * the OS version.
+   */
+
+  mprotect(p, sz, PROT_READ | PROT_WRITE);
+}
+
 __attribute__((constructor))
 static void save_and_replace_init_funcs(int argc, const char *argv[],
                                         const char *envp[],
                                         const char *apple[])
 {
+  init_func_t *fp;
+  unsigned int nfuncs = &mod_init_func_end - &mod_init_func_start;
+
+  assert(nfuncs > 0);
+  g_num_saved_init_funcs = nfuncs - 1;
+  if (g_num_saved_init_funcs == 0)
+    {
+      /* This function is the only constructor in the binary.
+       * no need to apply the following hack.
+       */
+
+      return;
+    }
+
   g_saved_argc = argc;
   g_saved_argv = argv;
   g_saved_envp = envp;
   g_saved_apple = apple;
-  init_func_t *fp;
-  unsigned int nfuncs = &mod_init_func_end - &mod_init_func_start;
-  assert(nfuncs > 1);
-  g_num_saved_init_funcs = nfuncs - 1;
+
   g_saved_init_funcs = malloc(g_num_saved_init_funcs *
                               sizeof(*g_saved_init_funcs));
+  allow_write(&mod_init_func_start, &mod_init_func_end);
   int i = 0;
   for (fp = &mod_init_func_start; fp < &mod_init_func_end; fp++)
     {
