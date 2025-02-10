bd62dacdeb5abf67ccd3eeb1337e701edd1b013f
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-02-10 23:01:33 +0900

    lto

--- wasm/longjmp/test-libsetjmp.sh
+++ wasm/longjmp/test-libsetjmp.sh
@@ -4,16 +4,20 @@ set -e
 
 ##CC=/opt/wasi-sdk-21.0/bin/clang
 CC="/Volumes/PortableSSD/llvm/build/bin/clang --sysroot /opt/wasi-sdk-24.0/share/wasi-sysroot -resource-dir /Volumes/PortableSSD/llvm/llvm/lib/clang/17"
+#CC=/Volumes/PortableSSD/git/wasi-sdk/build/install/bin/clang
 # binaryen with https://github.com/WebAssembly/binaryen/pull/6210
 WASM_OPT=~/git/wasm/binaryen/b/bin/wasm-opt
 # toywasm v36.0.0 or later with TOYWASM_ENABLE_WASM_EXCEPTION_HANDLING=ON
 TOYWASM=toywasm
 
 ${CC} \
+-flto=full \
 --target=wasm32-wasi \
 -mllvm -wasm-enable-sjlj \
+-Wl,-mllvm,-wasm-enable-sjlj \
 -DUSE_SETJMP_H \
--Os \
+-Os -v \
+-Wl,-mllvm,-debug \
 a.c -lsetjmp
 
 ${WASM_OPT} \
