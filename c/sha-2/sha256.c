0b596d63a02694bdc92ed1d45d1aeb9a15c0d85d
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-09-10 22:16:45 +0900

    sync with sha512.c

--- c/sha-2/sha256.c
+++ c/sha-2/sha256.c
@@ -12,56 +12,61 @@
 #include "sha256.h"
 #include "sha256_table.h"
 
-static uint32_t
-rotr(uint32_t v, unsigned int n)
+typedef uint32_t word;
+#define BLOCK_SIZE 64
+#define MSG_SIZE_SIZE 8
+#define MSG_SCHED_SIZE 64
+
+static word
+rotr(word v, unsigned int n)
 {
-        return (v >> n) | (v << (32 - n));
+        return (v >> n) | (v << (sizeof(word) * 8 - n));
 }
 
-static uint32_t
-shr(uint32_t v, unsigned int n)
+static word
+shr(word v, unsigned int n)
 {
         return v >> n;
 }
 
 /* 4.1.2 */
-static uint32_t
-ch(uint32_t x, uint32_t y, uint32_t z)
+static word
+ch(word x, word y, word z)
 {
         return (x & y) ^ (~x & z);
 }
 
 /* 4.1.2 */
-static uint32_t
-maj(uint32_t x, uint32_t y, uint32_t z)
+static word
+maj(word x, word y, word z)
 {
         return (x & y) ^ (x & z) ^ (y & z);
 }
 
 /* 4.1.2 */
-static uint32_t
-S0(uint32_t x)
+static word
+S0(word x)
 {
         return rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22);
 }
 
 /* 4.1.2 */
-static uint32_t
-S1(uint32_t x)
+static word
+S1(word x)
 {
         return rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25);
 }
 
 /* 4.1.2 */
-static uint32_t
-s0(uint32_t x)
+static word
+s0(word x)
 {
         return rotr(x, 7) ^ rotr(x, 18) ^ shr(x, 3);
 }
 
 /* 4.1.2 */
-static uint32_t
-s1(uint32_t x)
+static word
+s1(word x)
 {
         return rotr(x, 17) ^ rotr(x, 19) ^ shr(x, 10);
 }
@@ -90,20 +95,20 @@ be64_encode(uint8_t *p, uint64_t v)
 }
 
 static void
-init_w(const uint8_t *p, uint32_t w[64])
+init_w(const uint8_t *p, word w[MSG_SCHED_SIZE])
 {
         /* 6.2.2 1. */
         unsigned int i;
         for (i = 0; i < 16; i++) {
                 w[i] = be32_decode(&p[i * 4]);
         }
-        for (; i < 64; i++) {
+        for (; i < MSG_SCHED_SIZE; i++) {
                 w[i] = s1(w[i - 2]) + w[i - 7] + s0(w[i - 15]) + w[i - 16];
         }
 }
 
 static void
-update_h(const uint32_t w[64], uint32_t h[8])
+update_h(const word w[MSG_SCHED_SIZE], word h[8])
 {
         uint32_t t[8]; /* a,b,c,d,e,f,g,h */
         unsigned int i;
@@ -114,10 +119,9 @@ update_h(const uint32_t w[64], uint32_t h[8])
         }
 
         /* 6.2.2 3. */
-        for (i = 0; i < 64; i++) {
-                uint32_t t1 =
-                        t[7] + S1(t[4]) + ch(t[4], t[5], t[6]) + K[i] + w[i];
-                uint32_t t2 = S0(t[0]) + maj(t[0], t[1], t[2]);
+        for (i = 0; i < MSG_SCHED_SIZE; i++) {
+                word t1 = t[7] + S1(t[4]) + ch(t[4], t[5], t[6]) + K[i] + w[i];
+                word t2 = S0(t[0]) + maj(t[0], t[1], t[2]);
 
                 t[7] = t[6];
                 t[6] = t[5];
@@ -140,52 +144,22 @@ sha256_init(uint32_t h[8])
 {
 
         /* 5.3.3 */
-        memcpy(h, H, 32);
+        memcpy(h, H, sizeof(H));
 }
 
 void
-sha256_block(const void *p, uint32_t h[8])
+sha256_block(const void *p, word h[8])
 {
-        uint32_t w[64];
+        word w[64];
 
         init_w(p, w);
         update_h(w, h);
 }
 
 void
-sha256_tail(const void *p, size_t len, uint64_t total_len, uint32_t h[8])
+sha256_tail(const void *p, size_t len, uint64_t total_len, word h[8])
 {
-        uint8_t tmp[64];
-
-        assert(len < 64);
-        assert(total_len <= SHA256_MAX_MESSAGE_BYTES);
-        if (len > 64 - 8 - 1) {
-                /* no room for message length. needs two blocks */
-                memcpy(tmp, p, len);
-                tmp[len] = 0x80; /* 1-bit end of messgage + 7-bit padding */
-                memset(tmp + len + 1, 0, 64 - (len + 1)); /* padding */
-                len = 0;
-                sha256_block(tmp, h);
-                /* last block */
-                memset(tmp, 0, 64 - 8); /* padding */
-        } else {
-                /* last block */
-                memcpy(tmp, p, len);
-                tmp[len] = 0x80; /* 1-bit end of messgage + 7-bit padding */
-                memset(tmp + len + 1, 0, 64 - (len + 1) - 8); /* padding */
-        }
-        uint64_t bitlen = total_len * 8;
-        be64_encode(&tmp[64 - 8], bitlen);
-        sha256_block(tmp, h);
-}
-
-void
-sha256(const void *vp, size_t len, uint32_t h[8])
-{
-        const uint8_t *p = vp;
-        const uint64_t total_len = len;
-
-        sha256_init(h);
+        uint8_t tmp[BLOCK_SIZE];
 
         /*
          * 5.1.1
@@ -196,10 +170,40 @@ sha256(const void *vp, size_t len, uint32_t h[8])
          *
          * we store 8-byte message length at the end of the last block
          */
-        while (len >= 64) { /* full block */
+
+        assert(len < BLOCK_SIZE);
+        assert(total_len <= SHA256_MAX_MESSAGE_BYTES);
+        if (len > BLOCK_SIZE - MSG_SIZE_SIZE - 1) {
+                /* no room for message length. needs two blocks */
+                memcpy(tmp, p, len);
+                tmp[len] = 0x80; /* 1-bit end of messgage + 7-bit padding */
+                memset(tmp + len + 1, 0, BLOCK_SIZE - (len + 1)); /* padding */
+                len = 0;
+                sha256_block(tmp, h);
+                /* last block */
+                memset(tmp, 0, BLOCK_SIZE - MSG_SIZE_SIZE); /* padding */
+        } else {
+                /* last block */
+                memcpy(tmp, p, len);
+                tmp[len] = 0x80; /* 1-bit end of messgage + 7-bit padding */
+                memset(tmp + len + 1, 0,
+                       BLOCK_SIZE - (len + 1) - MSG_SIZE_SIZE); /* padding */
+        }
+        be64_encode(&tmp[64 - 8], total_len * 8);
+        sha256_block(tmp, h);
+}
+
+void
+sha256(const void *vp, size_t len, word h[8])
+{
+        const uint8_t *p = vp;
+        const uint64_t total_len = len;
+
+        sha256_init(h);
+        while (len >= BLOCK_SIZE) { /* full block */
                 sha256_block(p, h);
-                p += 64;
-                len -= 64;
+                p += BLOCK_SIZE;
+                len -= BLOCK_SIZE;
                 continue;
         }
         sha256_tail(p, len, total_len, h);
