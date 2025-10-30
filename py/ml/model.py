a1d5255c33dd98641b7d53d46c4ba71fbfa508df
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-10-30 20:02:36 +0900

    Revert "wip"
    This reverts commit f4209ab3f3ccfe2398647dea0acada444ed862cd.

--- py/ml/model.py
+++ py/ml/model.py
@@ -18,8 +18,8 @@ def dot(a, b):
 
 class Network:
     def __init__(self, sizes):
-        self.biases = [np.random.randn(y) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+        self.biases = [np.random.randn(1, y) for y in sizes[1:]]
+        self.weights = [np.random.randn(x, y) for x, y in zip(sizes[:-1], sizes[1:])]
 
 
 def assert_shape(a, shape):
@@ -27,9 +27,6 @@ def assert_shape(a, shape):
 
 
 def feed_forward_full(n, d):
-    # acts[0]: inputs of the network
-    # acts[l+1]: activations of layer l
-    # zs[l]: weighted inputs of layer l
     assert d.ndim == 2
     acts = []
     zs = []
@@ -37,10 +34,10 @@ def feed_forward_full(n, d):
     acts.append(a)
     for w, b in zip(n.weights, n.biases):
         (count, ninputs) = a.shape
-        noutputs = w.shape[0]
-        assert_shape(w, (noutputs, ninputs))
-        assert_shape(b, (noutputs,))
-        z = dot(w, a.T).T + b
+        noutputs = w.shape[1]
+        assert_shape(w, (ninputs, noutputs))
+        assert_shape(b, (1, noutputs))
+        z = dot(a, w) + b
         assert_shape(z, (count, noutputs))
         zs.append(z)
         a = sigmoid(z)
@@ -75,31 +72,24 @@ def test(n, data, answers):
 
 
 def assert_same_shape(la, lb):
-    for a, b in zip(la, lb):
-        assert_shape(a, b.shape)
+    #for a, b in zip(la, lb):
+    #    assert a.shape == b.shape
     pass
 
 
+def add_list(a, b):
+    assert_same_shape(a, b)
+    return [x + y for x, y in zip(a, b)]
+
+
 def sub_list_rate(a, b, rate):
-    #assert_same_shape(a, b)
-    return [x - rate * np.sum(y, axis=0) for x, y in zip(a, b)]
+    assert_same_shape(a, b)
+    return [x - rate * y for x, y in zip(a, b)]
 
 
 def cost_derivative(output, desired):
-    # partial derivative of our cost function.
-    #
-    # our cost function is quadratic cost function (aka MSE):
-    #    C = norm(desired - output) ^ 2 / 2
-    assert_shape(output, desired.shape)
+    # partial derivative of quadratic cost function
     return output - desired
-
-
-def list_dot(la, lb):
-    r = []
-    for ix, a in enumerate(la):
-        b = lb[ix]
-        r.append(dot(a.reshape(-1, 1), b.reshape(1, -1)))
-    return np.stack(r)
 
 
 def back_propagation(n, d, desired):
@@ -109,43 +99,31 @@ def back_propagation(n, d, desired):
     assert desired.shape[0] == count
 
     acts, zs = feed_forward_full(n, d)
-
-    # acts[0]: inputs of the network
-    # acts[l+1]: activations of layer l
-    # zs[l]: weighted inputs of layer l
+    assert_shape(acts[-1], desired.shape)
 
     n_w = [None] * len(n.weights)
     n_b = [None] * len(n.biases)
     assert len(acts) - 1 == len(zs) == len(n_w) == len(n_b)
 
-    # the cost function
-    assert_shape(acts[-1], desired.shape)
-    assert_shape(zs[-1], desired.shape)
-    # compute the error
     # note: "*" here is a hadamard product
     delta = cost_derivative(acts[-1], desired) * sigmoid_prime(zs[-1])
-    assert_shape(delta, desired.shape)
-    n_w[-1] = list_dot(delta, acts[-1 - 1])
-    #n_w[-1] = dot(delta, acts[-1 - 1].T)
+    assert delta.shape[0] == count
+    n_w[-1] = dot(delta, acts[-1 - 1].T)
     n_b[-1] = delta
-
-    # the rest of layers
     for l in range(2, len(acts)):
-        delta = dot(n.weights[-l + 1].T, delta.T).T * sigmoid_prime(zs[-l])
+        delta = dot(n.weights[-l + 1].T, delta) * sigmoid_prime(zs[-l])
         assert delta.shape[0] == count
-        n_w[-l] = list_dot(delta, acts[-l - 1])
-        #n_w[-l] = dot(delta, acts[-l - 1].T)
+        n_w[-l] = dot(delta, acts[-l - 1].T)
         n_b[-l] = delta
 
     return (n_w, n_b)
 
 
 def sgd(n, data, answers, rate):
-    #print(f"sgd data {data.shape} answers {answers.shape}")
     assert data.ndim == 2
     assert answers.ndim == 2
-    batch_size = data.shape[0]
-    assert answers.shape[0] == batch_size
+    assert data.shape[0] == answers.shape[0]
+    batch_size = data.shape[1]
     rate = rate / batch_size
     delta_n_w, delta_n_b = back_propagation(n, data, answers)
     n_w = delta_n_w
