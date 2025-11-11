6c41640646dc852c48123ab84434b8552cadb22f
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-11-12 00:22:18 +0900

    add a test

--- c/base64/test.sh
+++ c/base64/test.sh
@@ -8,3 +8,10 @@ cc ${DBG} -DTEST -o enc base64encode.c
 cc ${DBG} -DTEST -o dec base64decode.c
 
 printf "hello, base64\n" | ./enc | ./dec
+
+T=$(mktemp)
+T2=$(mktemp)
+dd if=/dev/urandom of=${T} count=100
+< ${T} ./enc | ./dec > ${T2}
+cmp ${T} ${T2}
+rm ${T} ${T2}
