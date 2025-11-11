3ea2f789cd6a56bac7f8653613aff7d3b1d8401d
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-11-11 23:37:57 +0900

    comment

--- c/base64/base64decode.h
+++ c/base64/base64decode.h
@@ -6,6 +6,10 @@ size_t base64decode_size(size_t srclen);
 
 /*
  * return the exact size of the decoded data
+ *
+ * this returns a large enough size even for an invalid base64 string.
+ * (the max possible bytes which base64decode can write out to
+ * the dst buffer before it detects the encoding error and returns -1.)
  */
 size_t base64decode_size_exact(const void *src, size_t srclen);
 
