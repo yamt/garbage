b702995d6459af445cafef65e67cae083e44afe7
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-11-17 23:56:30 +0900

    reduce code dup

--- c/base64/base64encode.c
+++ c/base64/base64encode.c
@@ -67,13 +67,9 @@ static uint8_t
 conv_to_char(uint8_t x)
 {
         BASE64_ASSUME(x < 64);
+#define E(a, b) [b] = a,
         static const char table[64] = {
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
-                '3', '4', '5', '6', '7', '8', '9', '+', '/',
+#include "base64_chars.h"
         };
         return table[x];
 }
