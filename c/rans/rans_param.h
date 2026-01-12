8853f4cc5382481883f9d5048cd2d6db8ebd4a8d
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-01-13 00:07:12 +0900

    rename things to match the paper

--- c/rans/rans_param.h
+++ c/rans/rans_param.h
@@ -18,13 +18,8 @@ typedef uint32_t rans_I;
 #define RANS_L (128 * RANS_M) /* l in the paper */
 #define RANS_M 65536          /* m in the paper */
 
-/*
- * I   = {L, ..., L * B - 1}
- * I_s = {L / M * l_s, ..., B * L / M * l_s - 1}
- */
-
-#define RANS_I_SYM_MIN(p_sym) ((rans_I)RANS_L / RANS_M * p_sym)
-#define RANS_I_SYM_MAX(p_sym) ((rans_I)RANS_B * RANS_L / RANS_M * p_sym - 1)
+#define RANS_I_SYM_MIN(l_s) ((rans_I)RANS_L / RANS_M * l_s)
+#define RANS_I_SYM_MAX(l_s) ((rans_I)RANS_B * RANS_L / RANS_M * l_s - 1)
 
 #define RANS_I_MIN ((rans_I)RANS_L)
 #define RANS_I_MAX ((rans_I)RANS_L * RANS_B - 1)
