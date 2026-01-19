7e81fa1f4008ec0ab25d06716c450bc816f48a23
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-01-19 21:26:17 +0900

    remove an unused argument

--- c/rans/rans_encode.c
+++ c/rans/rans_encode.c
@@ -37,7 +37,7 @@ rans_encode_set_extra(struct rans_encode_state *st, rans_I extra)
 }
 
 static void
-encode_normalize(struct rans_encode_state *st, rans_sym_t sym, rans_prob_t l_s,
+encode_normalize(struct rans_encode_state *st, rans_prob_t l_s,
                  struct bitbuf *bo)
 {
         assert(st->x >= RANS_I_SYM_MIN(l_s));
@@ -67,7 +67,7 @@ rans_encode_sym(struct rans_encode_state *st, rans_sym_t s, rans_prob_t b_s,
                 rans_prob_t l_s, struct bitbuf *bo)
 {
         assert(l_s > 0);
-        encode_normalize(st, s, l_s, bo);
+        encode_normalize(st, l_s, bo);
         rans_I q = st->x / l_s;
         rans_I r = st->x - q * l_s;
         rans_I newx = q * RANS_M + b_s + r;
