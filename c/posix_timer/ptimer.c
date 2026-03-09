cf7b893415080b177b5104ff6e8c75be5b57dc94
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-03-09 12:59:25 +0000

    timer_getoverrun in the signal handler

--- c/posix_timer/ptimer.c
+++ c/posix_timer/ptimer.c
@@ -10,12 +10,32 @@
 #include <unistd.h>
 
 sig_atomic_t got_sig;
+sig_atomic_t overruns;
+sig_atomic_t intervals;
 
 void
-sighandler(int sig)
+sighandler(int sig, siginfo_t *info, void *ctx)
 {
         assert(sig == SIGALRM);
+        assert(info->si_signo == SIGALRM);
+        assert(info->si_code == SI_TIMER);
+        if (info->si_errno != 0) {
+                fprintf(stderr, "si_errno=%d (%s)\n", info->si_errno,
+                        strerror(info->si_errno));
+                exit(1);
+        }
         got_sig++;
+        timer_t timer = *(const timer_t *)info->si_value.sival_ptr;
+        int ret = timer_getoverrun(timer);
+        if (ret == -1) {
+                fprintf(stderr,
+                        "timer_getoverrun failed, errno=%d "
+                        "(%s)\n",
+                        errno, strerror(errno));
+                exit(1);
+        }
+        overruns += ret;
+        intervals += ret + 1;
 }
 
 int
@@ -25,24 +45,34 @@ main(int argc, char **argv)
         struct itimerspec oit;
         int ret;
 
-        signal(SIGALRM, sighandler);
+        struct sigaction sa;
+        sa.sa_sigaction = sighandler;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = SA_SIGINFO;
+        ret = sigaction(SIGALRM, &sa, NULL);
+        if (ret != 0) {
+                fprintf(stderr, "sigaction failed, errno=%d (%s)\n", errno,
+                        strerror(errno));
+                exit(1);
+        }
 
         clockid_t id = CLOCK_REALTIME;
         timer_t timer;
         struct sigevent ev;
         ev.sigev_notify = SIGEV_SIGNAL;
         ev.sigev_signo = SIGALRM;
-        ev.sigev_value.sival_int = 1;
+        ev.sigev_value.sival_ptr = &timer;
         ret = timer_create(id, &ev, &timer);
         if (ret != 0) {
                 fprintf(stderr, "timer_create failed, errno=%d (%s)\n", errno,
                         strerror(errno));
                 exit(1);
         }
-        it.it_interval.tv_sec = 1;
-        it.it_interval.tv_nsec = 0;
-        it.it_value.tv_sec = 1;
-        it.it_value.tv_nsec = 0;
+        uint64_t interval_ns = 1000000000;
+        it.it_interval.tv_sec = interval_ns / 1000000000;
+        it.it_interval.tv_nsec = interval_ns % 1000000000;
+        it.it_value.tv_sec = interval_ns / 1000000000;
+        it.it_value.tv_nsec = interval_ns % 1000000000;
         ret = timer_settime(timer, 0, &it, &oit);
         if (ret != 0) {
                 fprintf(stderr, "timer_settime failed, errno=%d (%s)\n", errno,
@@ -65,19 +95,10 @@ main(int argc, char **argv)
                 if (n == 0) {
                         start_ns = now_ns;
                 } else {
-                        int overruns = timer_getoverrun(timer);
-                        if (overruns == -1) {
-                                fprintf(stderr,
-                                        "timer_getoverrun failed, errno=%d "
-                                        "(%s)\n",
-                                        errno, strerror(errno));
-                                exit(1);
-                        }
-                        printf("%u th wakeup: %12" PRIu64
-                               "ns from start. %10" PRIu64
-                               "ns from prev. overruns=%d\n",
+                        printf("%u: %12" PRIu64 "ns frm start, %10" PRIu64
+                               "ns frm prev, ov=%d, int=%d\n",
                                n, now_ns - start_ns, now_ns - prev_ns,
-                               overruns);
+                               (int)overruns, (int)intervals);
                 }
                 prev_ns = now_ns;
                 n++;
