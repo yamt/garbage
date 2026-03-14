a770c2b015ed30e883acd3f23377504f6a61437a
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-03-14 14:29:49 +0000

    a test program

---
+++ c/heap/heap.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+        if (argc != 2) {
+                fprintf(stderr, "args\n");
+                exit(2);
+        }
+        size_t sz = atol(argv[1]);
+        uint8_t *p = malloc(sz);
+        if (p == NULL) {
+                fprintf(stderr, "malloc failed\n");
+                exit(1);
+        }
+        size_t i;
+        for (i = 0; i < sz; i += 1024) {
+                *(size_t *)&p[i] = i;
+        }
+        for (i = 0; i < sz; i += 1024) {
+                size_t actual = *(size_t *)&p[i];
+                size_t expected = i;
+
+                if (i != actual) {
+                        fprintf(stderr,
+                                "offset %zu, expected %zx != actual %zx\n", i,
+                                expected, actual);
+                }
+        }
+}
