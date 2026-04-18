ae4ef427efeb63f42f39746ff08fc824c9f57dc4
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-04-18 13:00:41 +0900

    stop using -r, which macos version of install doesn't have

--- py/git-credential-oauth2/install.sh
+++ py/git-credential-oauth2/install.sh
@@ -5,5 +5,5 @@ set -e
 BINDIR=${BINDIR:-~/.local/bin}
 install -v -d ${BINDIR}
 for x in git-credential-oauth2-webapp git-credential-oauth2-device; do
-	install -v -r -m 555 "$@" $x.py ${BINDIR}/$x
+	install -v -m 555 "$@" $x.py ${BINDIR}/$x
 done
