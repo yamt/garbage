f8f43077c6ae3f73c33145464b32f797887dbb0e
YAMAMOTO Takashi <imuwoto@gmail.com>
2026-04-24 01:04:20 +0900

    zfs snapshot scripts

---
+++ netbsd/zfs/list_victims.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+# eg.
+# ./list_victims.sh x 'test-.*' 60 2
+
+set -e
+
+debug() {
+    #echo "# DEBUG: $@"
+}
+
+if [ $# -lt 4 ]; then
+    echo "usage: $0 ds snapname-regex threshold-age-in-sec min-keep"
+    exit 2
+fi
+
+DS=$1
+re=$2
+d=$3
+nkeep=$4
+
+now=$(date +%s)
+t=$((now - d))
+debug $t
+# note: -S creation outputs the latest snapshot first
+zfs list -t snap -o name,creation -S creation -Hprd1 ${DS} | while read s c; do
+    debug "checking $s $c"
+    snapname=${s#*@}
+    if ! echo ${snapname} | grep $re > /dev/null; then
+        debug "skip $s as snapname regex didn't match"
+        continue
+    fi
+    if [ $nkeep -gt 0 ]; then
+        debug "skip $s as it's one of latest snapshots"
+        nkeep=$((nkeep - 1))
+        continue
+    fi
+    if [ $c -gt $t ]; then
+        debug "skip $s as it's new"
+        continue
+    fi
+    echo $s
+done
---
+++ netbsd/zfs/periodic-snapshot.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+set -e
+
+B=$(readlink "$0" || echo "$0")
+D=$(cd ${B%/*} && pwd -P)
+
+debug() {
+    echo "# DEBUG: $@"
+}
+
+if [ $# -lt 4 ]; then
+    echo "usage: $0 ds prefix period-in-sec num-keep"
+    echo "note: negative num-keep means to skip gc"
+    exit 2
+fi
+
+DS=$1
+PREFIX=$2
+PERIOD=$3
+NKEEP=$4
+
+# take a new snapshot
+TS=$(date +%F-%H:%M:%S-%Z)
+SNAPNAME=${PREFIX}${TS}
+debug zfs snap -r ${DS}@${SNAPNAME}
+zfs snap -r ${DS}@${SNAPNAME}
+
+# remove old ones
+if [ "$NKEEP" -lt 0 ]; then
+    exit 0
+fi
+THRESH=$((NKEEP * PERIOD))
+${D}/list_victims.sh ${DS} "${PREFIX}.*" ${THRESH} ${NKEEP} | while read x; do
+    debug zfs destroy -r $x
+    zfs destroy -r $x
+done
